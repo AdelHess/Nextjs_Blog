877a671f6457eb257579907defdb7d06eae8d50a
AdelHess <insightsgrowthagency@gmail.com>
2026-09-07 02:25:03 -0400

    Delete project directory

--- project/app/sitemap.ts
+++
@@ -1,7 +0,0 @@
-import type { MetadataRoute } from 'next';
-import { getPosts } from '@/lib/posts';
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://yourdomain.com';
-  return [{ url: baseUrl, lastModified: new Date() }, ...getPosts().map((post) => ({ url: `${baseUrl}/posts/${post.slug}`, lastModified: new Date(post.date) }))];
-}
